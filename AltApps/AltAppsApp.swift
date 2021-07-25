9e04435cf8a7b3aaca764aabc4f024ebade88007
Jining Liu <Hbandtechnology@gmail.com>
2021-07-25 16:37:02 -0500

    Initial Commit

---
+++ AltApps/AltAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltAppsApp.swift
+//  AltApps
+//
+//  Created by JingJing on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AltAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
